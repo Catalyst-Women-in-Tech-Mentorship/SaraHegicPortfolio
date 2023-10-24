4348c99cfd4d262ed0107764bb1d4595560d8a35
Greenwoman1 <123844700+Greenwoman1@users.noreply.github.com>
2023-10-25 00:36:55 +0200

    update-home-page

--- portfolio/src/components/projectsGrid.js
+++ portfolio/src/components/projectsGrid.js
@@ -3,21 +3,46 @@ import Grid from "@mui/material/Grid";
 const ProjectsGrid = () => {
   return (
     <div className="projects-grid">
-        <Grid container justifyContent="center" spacing={4}>
-          
-            <Grid  item>
-                cvgubhjnk
-            </Grid>
-            
-            <Grid  item>
-                cvgubhjnk
-            </Grid>
-            
-            <Grid  item>
-                cvgubhjnk
-            </Grid>
-          
+      <Grid container justifyContent="center" spacing={12}>
+        <Grid item>
+          <div className="box">
+            <img src="../download.png" alt="Slika 1" />
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="text">Tekst 1</div>
+            </a>
+          </div>
         </Grid>
+
+        <Grid item>
+          <div className="box">
+            <img src="../download.png" alt="Slika 2" />
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="text">Tekst 2</div>
+            </a>
+          </div>
+        </Grid>
+
+        <Grid item>
+          <div className="box">
+            <img src="../download.png" alt="Slika 3" />
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="text">Tekst 3</div>
+            </a>
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 };
