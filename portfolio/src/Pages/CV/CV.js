5208a57346702478924491ea8306e1f0c578bc03
Greenwoman1 <123844700+Greenwoman1@users.noreply.github.com>
2023-11-21 15:37:25 +0100

    dev

--- portfolio/src/Pages/CV/CV.js
+++ portfolio/src/Pages/CV/CV.js
@@ -1,18 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 import Header from "../../components/Header/header";
 import "./CV.css"
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const CV = () => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
+const CV = () => {  
+  
+ 
   return (
     <div>
     <Header></Header>
@@ -20,9 +15,8 @@ const CV = () => {
         
       <Document
         file="../CV - Sara Hegić.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
       >
-        <Page pageNumber={pageNumber}
+        <Page pageNumber= {1}
         renderTextLayer={false}
         renderAnnotationLayer={false} />
       </Document>
